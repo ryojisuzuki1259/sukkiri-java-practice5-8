09af439dc62dd300d7d5a45e2a30cc88402906be
suzukiryoji <suzukiryoji@localhost>
2021-04-02 00:18:30 +0900

    answer

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void introduceOneself() {
+		String name = "鈴木";
+		int age = 28;
+		double height = 1.73;
+		char zodiac = '猿';
+		System.out.println("私の名前は" + name);
+		System.out.println("私の年齢は" + age);
+		System.out.println("私の身長は" + height);
+		System.out.println("私の干支は" + zodiac);
+	}
+
+	public static void main(String[] args) {
+		introduceOneself();
+
+	}
+
+}
