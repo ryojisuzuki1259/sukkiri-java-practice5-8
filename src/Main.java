cfc752f6ad3b1bc93d0d8f70717aea790ef017a7
suzukiryoji <suzukiryoji@localhost>
2021-04-02 00:30:10 +0900

    5-3answer

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,14 @@ public class Main {
 
 	}
 
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：無題");
+		System.out.println("本文："+ text);
+	}
+
 	public static void main(String[] args) {
-		email("メールのタイトル", "メールの宛先のアドレス", "メールの本文");
+		email("メールの宛先のアドレス", "メールの本文");
 
 	}
 
